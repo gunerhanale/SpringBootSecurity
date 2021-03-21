0a07317afe60b561245ec6c0fe6696e7b9b2a6b9
gunerhanale <gunerhanale@gmail.com>
2021-03-21 07:14:41 +0100

    created and implemented UserPrincipal Services

---
+++ forms/src/main/java/com/security/forms/security/UserPrincipalDetailsService.java
@@ -0,0 +1,24 @@
+package com.security.forms.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import com.security.forms.db.UserRepository;
+import com.security.forms.model.User;
+
+@Service
+public class UserPrincipalDetailsService implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String s) throws UsernameNotFoundException {
+        User user = this.userRepository.findByUsername(s);
+        UserPrincipal userPrincipal = new UserPrincipal(user);
+
+        return userPrincipal;
+    }
+}
